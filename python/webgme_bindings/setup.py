cee95ca33b359bb7db9aeed9c7485cfa5fe8af35
Patrik Meijer <patrik.meijer@vanderbilt.edu>
2018-09-04 09:25:54 -0500

    Python release 1.0.1

--- python/webgme_bindings/setup.py
+++ python/webgme_bindings/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="webgme_bindings",
-    version="1.0.0",
+    version="1.0.1",
     license='MIT',
     author="Patrik Meijer",
     author_email="webgme@vanderbilt.edu",
